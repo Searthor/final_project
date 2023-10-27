085ca26a7d7ed057ad0250f8ef1df592ca7b9da2
Searthor <searthor907@gmail.com>
2023-10-27 09:13:08 +0700

    sear

---
+++ app/Http/Livewire/ShowPosts.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowPosts extends Component
+{
+
+    public $name;
+
+    public function render()
+    {
+
+        if($this->name=="searthor"){
+            dd($this->name);
+        }
+
+
+
+        return view('livewire.show-posts')->layout('layouts.style');
+    }
+}
